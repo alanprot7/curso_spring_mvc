d3c332fb1607b1f173e406cf868b5cd29a0aac4c
Alan Oliveira <alanprot7@gmail.com>
2022-07-16 12:49:13 -0300

    Commit inicial

---
+++ src/main/java/br/com/homeoffice/registroescolar/RegistroescolarApplication.java
@@ -0,0 +1,13 @@
+package br.com.homeoffice.registroescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroescolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/homeoffice/registroescolar/RegistroescolarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.homeoffice.registroescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroescolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
