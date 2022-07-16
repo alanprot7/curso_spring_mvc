3d0e3a4aa10e505fe5c059a9449d9f481f166753
Alan Oliveira <alanprot7@gmail.com>
2022-07-16 15:47:44 -0300

    Video 4

--- src/main/java/br/com/homeoffice/registroescolar/controllers/HelloController.java
+++ src/main/java/br/com/homeoffice/registroescolar/controllers/HelloController.java
@@ -1,14 +1,32 @@
 package br.com.homeoffice.registroescolar.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@GetMapping("/hello")
-	public String hello() {
+	@GetMapping("/hello-servlet")
+	public String hello(HttpServletRequest request) {
+		request.setAttribute("nome", "Servlet");
 		return "hello";
+	}
+	
+	@GetMapping("/hello-model")
+	public String hello(Model model) {
+		model.addAttribute("nome", "Model");
+		return "hello";
+	}
+	
+	@GetMapping("/hello")
+	public ModelAndView hello() {
+		ModelAndView mv = new ModelAndView("hello");
+		mv.addObject("nome", "ModelAndView");
+		return mv;
 	}
 
 }
